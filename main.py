2d634d20d0c89f969cc64aad98d3131abc17bd2c
ne-urugal <monsterdog.vm@gmail.com>
2025-11-03 11:44:57 +0200

    Added main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+import math
+
+numbers = list(input())
+
+def square_sum(numbers):
+    for n in numbers:
+        sq_num = n ** 2
+        sum_sq = sq_num + sq_num
+        return sum_sq
+
+print(square_sum)
